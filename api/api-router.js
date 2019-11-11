362af8aaebfc936dda89445ef97f1185c486c6c3
seanaleid <seanaleid@gmail.com>
2019-11-11 12:36:11 -0500

    Pushes code from lecture, follow along

--- api/api-router.js
+++ api/api-router.js
@@ -3,11 +3,33 @@ const router = require('express').Router();
 const authRouter = require('../auth/auth-router.js');
 const usersRouter = require('../users/users-router.js');
 
+const bcrypt = require('bcryptjs');
+const Users = require('../users/users-model.js');
+
 router.use('/auth', authRouter);
 router.use('/users', usersRouter);
 
 router.get('/', (req, res) => {
   res.json({ api: "It's alive" });
 });
+
+router.post('/hash', (req, res) => {
+  // read a password from the body
+  // hash the password
+  // return it to the user in an object that looks like:
+  // { password: 'original password`, hash: `hashed password`}
+
+  const credentials = req.body;
+  const hash = bcrypt.hashSync(credentials.password, 14);
+  credentials.password = hash;
+
+  Users.add(user)
+    .then(saved => {
+      res.status(201).json(saved)
+    })
+    .catch(error => {
+      res.status(500).json(error)
+    })
+})
 
 module.exports = router;
